4556ae16cdd3835b11ef2c17435e7148b792df8c
Andreas Skoog <andreas.skoog@avanza.se>
2020-12-08 08:34:33 +0100

    refactor

--- src/main/java/se/askware/aoc2020/dec06/Dec06.java
+++ src/main/java/se/askware/aoc2020/dec06/Dec06.java
@@ -1,29 +1,23 @@
 package se.askware.aoc2020.dec06;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
+import se.askware.aoc2020.common.AocBase;
 
-public class Dec06 {
+public class Dec06 extends AocBase {
 
 	public static void main(String[] args) throws IOException {
-
-		List<String> readLines = IOUtils.readLines(Dec06.class.getResourceAsStream("inputs.txt"),
-				StandardCharsets.UTF_8);
-
-		part1(readLines);
-
-		part2(readLines);
+		new Dec06().run();
 	}
 
-	private static void part2(List<String> readLines) {
+	@Override
+	public void solvePartTwo(List<String> readLines) {
 		int sum = 0;
 		final List<String> current = new ArrayList<>();
-		for (String string : readLines) {
+		for (String string : input) {
 			if (string.trim().isEmpty()) {
 				int[] all = current.stream().flatMapToInt(String::chars).distinct().toArray();
 				long count = Arrays.stream(all).filter(c -> current.stream().allMatch(s -> s.indexOf(c) >= 0)).count();
@@ -39,10 +33,11 @@ public class Dec06 {
 		System.out.println(sum);
 	}
 
-	private static void part1(List<String> readLines) {
+	@Override
+	public void solvePartOne(List<String> input) {
 		int sum = 0;
 		String current = "";
-		for (String string : readLines) {
+		for (String string : input) {
 			if (string.trim().isEmpty()) {
 				sum += current.chars().distinct().count();
 				current = "";
